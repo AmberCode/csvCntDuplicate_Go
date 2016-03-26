3210ab1c5a97baf5c78748b3c244b41fce75e204
Andrius Urbelis <andrius.urbelis@gmail.com>
2016-03-26 09:23:53 +0200

    publish my first Go code

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+const fileName = "c:\\temp\\m2m_msisdns.csv"
+
+func main() {
+	log.Println("Start msisdn count")
+
+	start := time.Now()
+
+	hashTable := CountRepeats()
+
+	for key, value := range hashTable {
+		fmt.Println("Msisdn:", key, "Count rpt:", value)
+	}
+
+	elapsed := time.Since(start)
+	log.Printf("Took: %s", elapsed)
+	log.Println("End")
+}
+
+func CountRepeats() map[string]int {
+	hashTable := make(map[string]int)
+
+	f, err := os.Open(fileName)
+	defer f.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := csv.NewReader(bufio.NewReader(f))
+
+	for {
+		record, err := r.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+		}
+
+		result := strings.Split(record[0], ";")
+
+		if err != nil {
+			log.Println(err)
+			os.Exit(0)
+		}
+
+		hashTable[result[1]]++
+	}
+
+	return hashTable
+}
